cf5d7385a5978d935d980e91a67f26e1b92aa84e
Andrew Scott <andrewscott@email.wcu.edu>
2018-10-22 11:00:11 -0400

    Modifed sum and moved logs

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function myFunction(valueA, valueB){
-	var x = valueA + valueB;
-	console.log(x);
+	var x = valueA + valueB + 2;;
 	return x;
 }
 
 var y = myFunction(10,50);
+console.log("Hello");
 console.log(y);
